aa6ac208e7b8bf7c2940f086dc30bf1502cc448b
lcs <cslee@itmoulds.com>
2020-12-23 10:45:27 +0900

    Added - lazy_loading - 레이지 로딩 추가

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,20 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+
 
 Vue.use(Router)
 
+function load(componentPath){
+  return import(`@/views/${componentPath}`)
+}
+
 export default new Router({
+  mode : 'history',
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Main',
+      component: () => load('Main.vue')
     }
   ]
 })
--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,14 +1,13 @@
 <template>
-  <div id="app">
-    <router-view/>
-  </div>
+  <h1>Hello Vue.js</h1>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: "Main"
 }
 </script>
 
-<style>
+<style scoped>
+
 </style>
